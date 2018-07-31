3be359c38f7b17aeb6ea2cb3dd305878bc657ffa
someone <something@apa.com>
2018-07-31 14:27:30 -0400

    add a driver with test class

---
+++ src/main/kotlin/Driver.kt
@@ -0,0 +1,55 @@
+package org.jtodd.kvend.vend
+
+import java.io.InputStream
+import java.io.PrintStream
+import java.util.Scanner
+
+class Driver(input: InputStream, private val output: PrintStream) {
+
+    private val scanner = Scanner(input)
+    private val stock: Map<Product, Int> = mapOf(ProductImpl.Cola to 10, ProductImpl.Chips to 10, ProductImpl.Candy to 10)
+    private val bank: Map<Coin, Int> = mapOf(CoinImpl.Quarter to 10, CoinImpl.Dime to 10, CoinImpl.Nickel to 10)
+    private val machine = VendingMachine(stock, bank)
+
+    fun run() {
+        displayOptions(output)
+        var pair = getOption(scanner)
+        var command = pair.component1()
+        var argument = pair.component2()
+        while (command != 'X') {
+            when (command) {
+                'D' -> output.println(machine.display())
+                else -> {
+                    output.println("Unrecognized command")
+                }
+            }
+            displayOptions(output)
+            pair = getOption(scanner)
+            command = pair.component1()
+            argument = pair.component2()
+        }
+    }
+
+    private fun displayOptions(output: PrintStream) {
+        output.println("D: Check display")
+        output.println("I[PNDQHD(S%.4f,%.4f)]: Insert (penny, nickel, dollar, quarter, half-dollar, dollar, slug(diameter, mass)")
+        output.println("S[123]: Select (1 = chips, 2 = candy, 3 = soda")
+        output.println("C: Cancel purchase")
+        output.println("R: Check for change")
+        output.println("P: Check for product")
+        output.println("X: Exit")
+        output.println()
+        output.print("Choice: ")
+    }
+
+    private fun getOption(scanner: Scanner): Pair<Char, String> {
+        val choice: String = scanner.nextLine()
+        val option: Char = choice[0].toUpperCase()
+        val arg: String = choice.substring(1).toLowerCase()
+        return option to arg
+    }
+}
+
+fun main(args: Array<String>) {
+    Driver(System.`in`, System.out).run()
+}
---
+++ src/test/kotlin/DriverTest.kt
@@ -0,0 +1,59 @@
+package org.jtodd.kvend.vend
+
+import java.io.ByteArrayOutputStream
+import java.io.PrintStream
+
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
+
+import org.junit.Before as before
+import org.junit.Test as test
+
+class DriverTest {
+
+    val ls = System.getProperty("line.separator")
+    val expectedSplash = """D: Check display
+                           |I[PNDQHD(S%.4f,%.4f)]: Insert (penny, nickel, dollar, quarter, half-dollar, dollar, slug(diameter, mass)
+                           |S[123]: Select (1 = chips, 2 = candy, 3 = soda
+                           |C: Cancel purchase
+                           |R: Check for change
+                           |P: Check for product
+                           |X: Exit
+                           |
+                           |Choice: """.trimMargin("|")
+    lateinit var baos: ByteArrayOutputStream
+    lateinit var output: PrintStream
+
+    @before
+    fun setUp() {
+        baos = ByteArrayOutputStream()
+        output = PrintStream(baos)
+    }
+
+    @test
+    fun `prints options upon startup`() {
+        val input = "X".byteInputStream()
+
+        Driver(input, output).run()
+
+        assertEquals(expectedSplash, baos.toString(), "Wrong splash generated")
+    }
+
+    @test
+    fun `prints warning when an unrecognized command is entered`() {
+        val input = "Y${ls}X".byteInputStream()
+
+        Driver(input, output).run()
+
+        assertTrue(baos.toString().contains("Unrecognized command"), "User should be warned when an unrecognized command is entered")
+    }
+
+    @test
+    fun `gets machine display`() {
+        val input = "D${ls}X".byteInputStream()
+
+        Driver(input, output).run()
+
+        assertTrue(baos.toString().contains(VendingMachine.INSERT_COIN), "Machine should return display when requested")
+    }
+}
