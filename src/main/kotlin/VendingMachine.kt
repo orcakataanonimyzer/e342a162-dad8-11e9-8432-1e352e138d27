8d49e77319fb5999b5c6377bb0fb1380a4decfed
someone <something@apa.com>
2018-08-02 11:57:26 -0400

    Refactor
    * Ensure that bank only retains acceptable coins
    * Remove underscores from names of class fields

--- src/main/kotlin/VendingMachine.kt
+++ src/main/kotlin/VendingMachine.kt
@@ -2,6 +2,15 @@ package org.jtodd.kvend.vend
 
 class VendingMachine(stock: Map<Product, Int>, bank: Map<Coin, Int>) {
 
+    private val stock = stock.toMutableMap()
+    private val bank: MutableMap<Coin, Int> = acceptableCoins.associate { it to 0 }.toMutableMap()
+    private val acceptedCoinList = mutableListOf<Coin>()
+    private val acceptedValue
+        get() = acceptedCoinList.map { it -> it.monetaryValue }.sum()
+    private val coinReturn = mutableListOf<Coin>()
+    private var tempMessage = ""
+    private var lifetime = 0
+
     init {
         if (stock.values.any { it < 0 }) {
             throw IllegalArgumentException("No product can have a negative stock")
@@ -9,27 +18,19 @@ class VendingMachine(stock: Map<Product, Int>, bank: Map<Coin, Int>) {
         if (bank.values.any { it < 0 }) {
             throw IllegalArgumentException("No coin in the bank can have a negative count")
         }
+        bank.filter { (k, v) -> k in acceptableCoins }.forEach { (k, v) -> this.bank[k] = v }
     }
 
-    private val _stock = stock.toMutableMap()
-    private val _bank = bank.toMutableMap()
-    private val acceptedCoinList = mutableListOf<Coin>()
-    private val _acceptedValue
-        get() = acceptedCoinList.map { it -> it.monetaryValue }.sum()
-    private val _coinReturn = mutableListOf<Coin>()
-    private var _tempMessage = ""
-    private var _lifetime = 0
-
     fun display(): String {
-        if (_lifetime > 0) {
-            --_lifetime
-            return _tempMessage
+        if (lifetime > 0) {
+            --lifetime
+            return tempMessage
         } else if (!canMakeChange()) {
             return EXACT_CHANGE_ONLY
-        } else if (_acceptedValue == 0) {
+        } else if (acceptedValue == 0) {
             return INSERT_COIN
         } else {
-            return formatValue(_acceptedValue)
+            return formatValue(acceptedValue)
         }
     }
 
@@ -37,38 +38,38 @@ class VendingMachine(stock: Map<Product, Int>, bank: Map<Coin, Int>) {
         if (acceptableCoins.any { matchCoins(coin, it) } ) {
             acceptedCoinList.add(coin)
         } else {
-            _coinReturn.add(coin)
+            coinReturn.add(coin)
         }
     }
 
     fun setDisplayWithLifetime(message: String, lifetime: Int) {
-        _tempMessage = message
-        _lifetime = lifetime
+        tempMessage = message
+        this.lifetime = lifetime
     }
 
     fun buy(product: Product) {
-        if (product.price > _acceptedValue) {
+        if (product.price > acceptedValue) {
             setDisplayWithLifetime(message = formatPrice(product.price), lifetime = 1)
-        } else if (!_stock.containsKey(product) || _stock[product]!! < 1) {
+        } else if (!stock.containsKey(product) || stock[product]!! < 1) {
             setDisplayWithLifetime(message = SOLD_OUT, lifetime = 1)
         } else {
-            acceptedCoinList.forEach { it -> _bank[it] = _bank[it]!! + 1 }
+            acceptedCoinList.forEach { it -> bank[it] = bank[it]!! + 1 }
             acceptedCoinList.clear()
-            _coinReturn.addAll(makeChange(amount = _acceptedValue - product.price))
-            _stock[product] = _stock[product]!! - 1
+            coinReturn.addAll(makeChange(amount = acceptedValue - product.price))
+            stock[product] = stock[product]!! - 1
             setDisplayWithLifetime(message = THANK_YOU, lifetime = 1)
         }
     }
 
-    fun coinReturn() = _coinReturn.toList()
+    fun coinReturn() = coinReturn.toList()
 
     fun cancel() {
-        _coinReturn.addAll(acceptedCoinList)
+        coinReturn.addAll(acceptedCoinList)
         acceptedCoinList.clear()
     }
 
     fun canMakeChange() =
-            acceptableCoins.map { _bank.getOrDefault(it, 0) }.none { it < 1 }
+            bank.values.none { it < 1 }
 
     fun makeChange(amount: Int): List<Coin> {
         val returnList = mutableListOf<Coin>()
@@ -78,7 +79,7 @@ class VendingMachine(stock: Map<Product, Int>, bank: Map<Coin, Int>) {
             var __amount = amount
             while (__amount >= coin.monetaryValue) {
                 list.add(coin)
-                _bank[coin] = _bank[coin]!! - 1
+                bank[coin] = bank[coin]!! - 1
                 __amount -= coin.monetaryValue
             }
             return __amount
